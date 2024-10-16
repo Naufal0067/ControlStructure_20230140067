282a79e92cebbbd34cbda088cf3134084ba176a8
Naufal0067 <naufalopang1928@gmail.com>
2024-10-16 20:22:31 +0700

    first commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+# Program untuk mencetak pola berdasarkan input pengguna
+
+# Meminta input dari pengguna
+n = int(input("Masukkan nilai n: "))
+
+# Perulangan luar untuk menangani baris (dari 1 hingga n)
+for i in range(1, n + 1):
+    # Perulangan dalam untuk mencetak angka 'i' sebanyak 'i' kali
+    for j in range(i):
+        print(i, end=" ")  # Mencetak angka 'i' diikuti dengan spasi
+    print()  # Berpindah ke baris berikutnya setelah setiap baris selesai
---
+++ tugasno1.py
@@ -0,0 +1,12 @@
+def evaluate_performance(percentage):
+    if percentage >= 90:
+        print ("Execellent perfromance")
+    elif percentage >= 80:
+        print ("Very Good performance")
+    elif percentage >= 70:
+        print ("Good performance")
+    elif percentage >= 60:
+        print ("Average performance")
+    else:
+        print ("Below average performance")
+    
